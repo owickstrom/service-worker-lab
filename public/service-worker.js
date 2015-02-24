b05280f24cfe57fb302c5674c871a9c4d910931d
Oskar Wickström <oskar.wickstrom@live.com>
2015-02-24 21:22:31 +0100

    Upgrade caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@
 
 importScripts('serviceworker-cache-polyfill.js');
 
-var STATIC = 'static-1.0.0';
-var DYNAMIC = 'dynamic-1.0.0';
+var STATIC = 'static-v2';
+var DYNAMIC = 'dynamic-v2';
 var CACHE_NAMES = [STATIC, DYNAMIC];
 
 self.addEventListener('install', function (event) {
